b54c2f698f0fd601365321a2427515a01bea3b47
Ishwarya-arumugam <ishwaryaa44@gmail.com>
2016-06-29 10:57:49 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+int  a,b,i,c[50];
+clrscr();
+printf("Enter the number");
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&c[i]);
+scanf("%d",&b);
+printf("%d",c[b]);
+}
+getch();
+}
